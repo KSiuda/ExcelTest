9e88bbe18d3a672ef1e7e40399fd0d307d5014f2
Konrad Siuda <konrad.siuda@gmail.com>
2019-03-25 18:50:39 +0100

    Fixed!!!

--- ExcelTest/Program.cs
+++ ExcelTest/Program.cs
@@ -32,34 +32,41 @@ namespace ExcelTest
 
                 int rowCount = xlRange.Rows.Count;
                 int colCount = xlRange.Columns.Count;
-                var strings = new List<Tuple<int, string, string, string>>();
+                var trainings = new List<Tuple<int, string, string, string>>();
+                var consultants = new List<Tuple<int,string>>();
+                var trainingrecord = new List<Tuple<int, string>>();
+                var blanks = new List<int>();
+                int blank = 0;
+
 
             //iterate over the rows and columns and print to the console as it appears in the file
             //excel is not zero based!!
-                for (int row = 3; row <= rowCount; row++)
+            for (int row = 3; row <= rowCount; row++)
                 {
                     string training = "";
                     string instructor = "";
                     string duration = "";
 
-                    for (int column = 2; column <= 7; column++)
+                    for (int column = 2; column < 8; column++)
                     {
                        
                         if (column == 2)
                         {
                             if (xlRange.Cells[row, column] != null && xlRange.Cells[row, column].Value2 != null)
                             {
-                                int index0 = row - 3; //since we read from the 3rd row and we want to count from 0
-                                int index1 = column - 2; //since we read from the 3rd column, and we want to count from 1
 
                                 string value = xlRange.Cells[row, column].Value2.ToString();
                                 if (value.ToUpper() != value)
                                 {
                                     training = value;
                                 }
-                            
+                                else
+                                {
+                                    blanks.Add(row);
+                                }
 
                             }
+
                         }
                        
                         if (column == 6)
@@ -73,26 +80,24 @@ namespace ExcelTest
                                 {
                                     instructor = value;
                                 }
-                            
 
                             }
+                             
                         }
 
-                    if (column == 7)
-                    {
-
-                        if (xlRange.Cells[row, column] != null && xlRange.Cells[row, column].Value2 != null)
+                        if (column == 7)
                         {
-                            string value = xlRange.Cells[row, column].Value2.ToString();
 
-                            if (value.ToUpper() != value)
+                            if (xlRange.Cells[row, column] != null && xlRange.Cells[row, column].Value2 != null)
                             {
-                                duration = value;
+                                string value = xlRange.Cells[row, column].Value2.ToString();
+                               
+                                    duration = value;
+
                             }
-
-
                         }
-                    }
+
+
                 }
 
                 if (instructor != "" || training != "")
@@ -101,20 +106,92 @@ namespace ExcelTest
                     {
                         instructor = "None";
                     }
-                    strings.Add(Tuple.Create(strings.Count() + 1, training, instructor, duration));
+                    if(duration == "")
+                    {
+                        duration = null;
+                    }
+                    trainings.Add(Tuple.Create(trainings.Count() + 1, training, instructor, duration));
                 }
             }
 
 
-            foreach (var tuple in strings)
+
+
+            for (int row = 1; row <= 1; row++)
             {
-                Console.Write($" {tuple.Item1}, {tuple.Item2}, {tuple.Item3}, {tuple.Item4}");
-                Console.WriteLine();
+                string consultant = "";
+
+                for (int column = 10; column <= colCount; column++)
+                {
+                    string value = xlRange.Cells[row, column].Value2.ToString();
+
+                    consultant = value;
+                    consultants.Add(Tuple.Create(consultants.Count() + 1, consultant));
+                }
+                  
+            }
+
+            
+            foreach(var consultant in consultants)
+            {
+                blank = 0;
+                for (int row = 3; row <= trainings.Count() + 2 + blanks.Count(); row++)
+                {
+                    string trainingState = "";
+                    int counter = row - 2;
+
+                    for (int column = 10 + consultant.Item1 - 1; column < 10 + consultant.Item1; column++)
+                    {
+                        if (xlRange.Cells[row, column] != null && xlRange.Cells[row, column].Value2 != null)
+                        {
+
+                            string value = xlRange.Cells[row, column].Value2.ToString();
+                           
+                                trainingState = value;
+                                if(trainingState == "")
+                                {
+                                    blank++;
+                                }
+                                else
+                                {
+                                    trainingrecord.Add(Tuple.Create((counter - blank), trainingState));
+                                }
+                            
+                        }
+                        else
+                        {
+                            blank++;
+                        }
+                    }
+
+                }
+            }
+               
+            
+
+
+
+
+            Console.WriteLine("Trainings:");
+            foreach (var training in trainings)
+            {
+                Console.WriteLine($" {training.Item1}, {training.Item2}, {training.Item3}, {training.Item4}");
+               
+            }
+            Console.WriteLine("Consultants");
+            foreach (var consultant in consultants)
+            {
+                Console.WriteLine($" {consultant.Item1}, {consultant.Item2}");
+            }
+            Console.WriteLine("Training State for Marcin");
+            foreach (var training in trainingrecord)
+            {
+                Console.Write($" {training.Item1} {training.Item2},");
             }
 
 
-                //cleanup
-                GC.Collect();
+            //cleanup
+            GC.Collect();
                 GC.WaitForPendingFinalizers();
 
                 //rule of thumb for releasing com objects:
